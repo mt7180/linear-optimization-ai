9a655eb3ce9767912e4e8e881278c42a42dcb76d
Mira <mira.theidel@gmx.de>
2024-08-28 18:52:23 +0200

    enhance check_if_expression_is_safe function to accept more mathematical operations, needed for the objective and constraint rules/ expressions

--- src/llm_optimizer/utils/helpers.py
+++ src/llm_optimizer/utils/helpers.py
@@ -1,17 +1,18 @@
 import ast
 import logging
+from typing import Collection, Any
 
 class ExpressionNotSafeError(Exception):
     pass
 
-def check_if_expression_is_safe(expr, allowed_vars):
+def check_if_expression_is_safe(expr: str, allowed_vars: Collection[str]) -> bool:
     allowed_vars = set(allowed_vars)
-    allowed_math_functions = {'sum'}
+    allowed_math_functions = {'sum', 'ord', 'range'}
    
     allowed_node_types = {
         ast.Expression, ast.BinOp, ast.UnaryOp, ast.Constant, ast.Name,
         ast.Add, ast.Sub, ast.Mult, ast.Div, ast.FloorDiv, ast.Mod, ast.Pow,
-        ast.Eq, ast.Gt, ast.Lt, ast.GtE, ast.LtE, ast.Compare, ast.Call, ast.Tuple, #ast.Load,
+        ast.Eq, ast.Gt, ast.Lt, ast.GtE, ast.LtE, ast.Compare, ast.Call, ast.Tuple,
         ast.List, ast.Constant, ast.Expr, ast.Attribute, ast.GeneratorExp, 
         ast.comprehension, ast.Subscript, ast.IfExp
     }
@@ -23,29 +24,43 @@ def check_if_expression_is_safe(expr, allowed_vars):
             logging.debug(f"Node type '{type(node)}' not allowed.")
             return False
         
-        if isinstance(node, ast.BinOp) or isinstance(node, ast.UnaryOp):
-            logging.debug("checking operations ...")
+        if isinstance(node, ast.BinOp):
+            logging.debug("checking binary operations ...")
             return _check_node(node.left) and _check_node(node.right)
+        
+        if isinstance(node, ast.UnaryOp):
+            logging.debug("checking unary operations ...")
+            return _check_node(node.operand)
 
         if isinstance(node, ast.Call):
-            logging.debug(f"checking function calls (e.g., sum): {node.func.value}")
-            logging.debug(f"{node.func.id} ...")
+            logging.debug("checking function calls (e.g., sum): ")
+            #logging.debug(f"{node.func.id} ...")
+            if isinstance(node.func, ast.Name): 
+                return (
+                    node.func.id in allowed_math_functions and
+                    all(_check_node(arg) for arg in node.args)
+                )
             return (
-                isinstance(node.func, ast.Name) and
-                node.func.id in allowed_math_functions and
+                isinstance(node.func, ast.Attribute) and
+                _check_node(node.func) and
                 all(_check_node(arg) for arg in node.args)
             )
         
         if isinstance(node, ast.Name):
             logging.debug(f"checking variable: ")
-            logging.debug("{node.id}")
+            logging.debug(f"{node.id}")
             return node.id in allowed_vars or node.id in iteration_vars
         
         if isinstance(node, ast.Attribute):
             logging.debug(f"checking attributes: ")
-            full_name = f"{node.value.id}.{node.attr}"
-            logging.debug(f" {full_name} ...")
-            return full_name in allowed_vars
+            if isinstance(node.value, ast.Attribute):
+                return _check_node(node.value)
+            elif isinstance(node.value, ast.Name):
+                full_name = f"{node.value.id}.{node.attr}"
+                # logging.debug(f" {full_name} ...")
+                return full_name in allowed_vars
+            logging.debug(f"attibute value {node.value} not allowed")
+            return False
 
         if isinstance(node, ast.Constant):
             logging.debug("checking literals: {node.value} ...")
@@ -72,6 +87,10 @@ def check_if_expression_is_safe(expr, allowed_vars):
                     for elt in gen.target.elts:
                         if isinstance(elt, ast.Name):
                             iteration_vars.add(elt.id)
+            return (
+                _check_node(node.elt) and
+                all(_check_node(gen.iter) for gen in node.generators)
+            )
         
         if isinstance(node, ast.Subscript):
             logging.debug("checking subscripts like x[k] ...")
@@ -89,16 +108,15 @@ def check_if_expression_is_safe(expr, allowed_vars):
         return True
     
     try:
-    
         ast_tree = ast.parse(expr, mode='eval')
         return _check_node(ast_tree.body)
     except Exception as e:
         print(f"Exception occured while checking the expression: {e}")
         return False
 
-def parse_rule(expression: str, allowed_vars: list[str]):
+def parse_rule(expression: str, allowed_vars: Collection[str]) -> dict[str, Any]:
     """check and eval mathematical expression e.g. for pyomo rule"""
-    logging.debug(f"{expression=}, {allowed_vars}")
+    logging.debug(f"{expression=}, {allowed_vars=}")
 
     if "lambda" in expression:
         expr_left, expr_right = expression.split(":", 1)
@@ -106,13 +124,16 @@ def parse_rule(expression: str, allowed_vars: list[str]):
         expression = expr_right.strip()
     else: lambda_args=["model"]
         
-    expression_is_safe = check_if_expression_is_safe(expression, allowed_vars | set(lambda_args))
+    expression_is_safe = check_if_expression_is_safe(expression, set(allowed_vars) | set(lambda_args))
     if not expression_is_safe:
-        raise ExpressionNotSafeError()
-    print("set: ", set(lambda_args) - {"model"})
+        raise ExpressionNotSafeError(f"{expression=}, {allowed_vars=}")
+
+    args = set(lambda_args) - {"model"}
+    logging.debug(f'{args=} { ",".join(lambda_args)}')
     return {
         "func": eval("lambda " + ",".join(lambda_args) + ":" + expression),
-        "idx": set(lambda_args) - {"model"},
+
+        "args": args,
     }
 
 
