d699c95a25ddb8df349b01665b16f87bf27f0fe2
Mira <mira.theidel@gmx.de>
2024-09-19 12:54:07 +0200

    refactor llm communication

--- src/llm_optimizer/llm/communication_instructor.py
+++ src/llm_optimizer/llm/communication_instructor.py
@@ -17,20 +17,12 @@ logging.getLogger("openai._base_client").setLevel(logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
 
 
-def generate_pyomo_model(
+def get_llm_pyomo_model(
+    client: instructor.Instructor,
     user_input: str,
     validate_input: bool = True,
+    llm_prompt_settings=dict(temperature=0.2, max_tokens=2048),
 ) -> LinearOptimizationModel:
-    client: instructor.Instructor = instructor.from_openai(
-        openai.OpenAI(api_key=SETTINGS.OPENAI_API_KEY),
-        mode=instructor.Mode.JSON,  # .TOOLS,
-    )
-
-    llm_prompt_settings = {
-        "temperature": 0.2,
-        "max_tokens": 2048,
-    }
-
     if validate_input:
         is_optimization_problem = validate_optimization_problem(user_input, client)
         logging.debug(
@@ -42,12 +34,47 @@ def generate_pyomo_model(
                 f"Optimization problem not valid, reason: {is_optimization_problem.reason}"
             )
 
-    pyomo_model: LinearOptimizationModel = extract_pyomo_model(
-        user_input, client, llm_prompt_settings
+    prompt = inspect.cleandoc(f'''
+        You are an AI assistant tasked with transfering a clients linear 
+        optimization task into a mathematical formulation and a 
+        python/ pyomo code snippet. Please use
+        only the following pyomo modeling components to carefully define
+        the pyomo model for the given optimization task. Please try to
+        make the model as compact and pythonic as possible while staying 
+        mathematically correct. Use the `pyomo.Param()` only if absolutely necessary.
+
+        pyomo modeling components: """                     
+        - A pyomo ConcreteModel, the pyomo model instance needs to be named `model`.               
+        - `pyomo.Set()`               
+        - `pyomo.Var()`                    
+        - `pyomo.Param()` 
+        - `pyomo.Constraint()`                    
+        - for any rule only python lambda functions should be used.
+        """
+        
+        Please be thorough and provide the response in the given response format. 
+        Remember to start pyomo indexes with 1, not 0.
+                                                
+        optimization task: """
+        {user_input}
+        """
+    ''')
+
+    pyomo_model: LinearOptimizationModel = client.chat.completions.create(
+        max_retries=2,
+        model="gpt-4o",
+        response_model=LinearOptimizationModel,
+        max_tokens=llm_prompt_settings.get("max_tokens", 1024),
+        temperature=llm_prompt_settings.get("temperature", 0.2),
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
     )
     pyomo_model.problem_str = user_input
 
-    # logging.debug(pyomo_model.model_dump_json(indent=2))
     return pyomo_model
 
 
@@ -77,57 +104,6 @@ def validate_optimization_problem(
     )
 
 
-def extract_pyomo_model(
-    user_input: str,
-    client: instructor.Instructor,
-    settings: dict,
-) -> LinearOptimizationModel:
-    prompt = inspect.cleandoc(f'''
-        You are an AI assistant tasked with transfering a clients linear 
-        optimization task into a mathematical formulation and a 
-        python/ pyomo code snippet. Please use
-        only the following pyomo modeling components to carefully define
-        the pyomo model for the given optimization task. Please try to
-        make the model as compact and pythonic as possible while staying 
-        mathematically correct. Use the `pyomo.Param()` only if absolutely necessary.
-
-        pyomo modeling components: """                     
-        - A pyomo ConcreteModel, the pyomo model instance needs to be named `model`.               
-        - `pyomo.Set()`               
-        - `pyomo.Var()`                    
-        - `pyomo.Param()` 
-        - `pyomo.Constraint()`                    
-        - for any rule only python lambda functions should be used.
-        """
-        
-        Please be thorough and provide the response in the given response format. 
-        Remember to start pyomo indexes with 1, not 0.
-                                                
-        optimization task: """
-        {user_input}
-        """
-    ''')
-
-    # Also try to use integer values for the pyomo set(s) to make it easier to formulate rules and expressions.
-    # - You can assume, that pyomo is imported as `pyomo`.
-    # You are a world class optimization and pyomo expert. Give your best in
-    #         creating a
-
-    return client.chat.completions.create(
-        max_retries=2,
-        model="gpt-4o",
-        response_model=LinearOptimizationModel,
-        max_tokens=settings.get("max_tokens", 1024),
-        temperature=settings.get("temperature", 0.2),
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-    )
-
-
 def ask_llm_for_pyomo_model(
     problem_formulation: str,
     validate_input: bool = True,
@@ -141,27 +117,38 @@ def ask_llm_for_pyomo_model(
         cwd = Path(__file__).parent.parent.parent.parent
         with open(cwd / "tests" / "mock_llm_response_complex.json", "r") as file:
             mocked_response = LinearOptimizationModel(**json.load(file))
+
         return mocked_response
+
+    client: instructor.Instructor = instructor.from_openai(
+        openai.OpenAI(api_key=SETTINGS.OPENAI_API_KEY),
+        mode=instructor.Mode.JSON,  # .TOOLS,
+    )
+
+    llm_prompt_settings = {
+        "temperature": 0.2,
+        "max_tokens": 2048,
+    }
 
     for _ in range(max_retries):
         try:
-            structured_response = generate_pyomo_model(
-                problem_formulation, validate_input=validate_input
+            llm_pyomo_model = get_llm_pyomo_model(
+                client,
+                problem_formulation,
+                validate_input=validate_input,
+                llm_prompt_settings=llm_prompt_settings,
             )
-            logging.debug(structured_response.model_dump_json(indent=2))
+            logging.debug(llm_pyomo_model.model_dump_json(indent=2))
         except ValidationError as e:
             logging.debug(e.args)
             return LinearOptimizationModel()
         else:
             break
 
-    return structured_response
+    return llm_pyomo_model
 
 
 if __name__ == "__main__":
-    import os
-    from pathlib import Path
-
     post_office_optimization_task = """A post office requires a different number of
     full-time employees on each day of the week. The number of full-time employees
     required for each day is: Monday= 17, Tuesday= 13, Wednesday= 15, Thursday= 19, Friday= 14, Saturday= 16, Sunday= 11.
@@ -174,16 +161,11 @@ if __name__ == "__main__":
     What should Max eat so as to get the most points?
     """
 
-    try:
-        pyomo_model = generate_pyomo_model(
-            post_office_optimization_task, validate_input=False
-        )
-        print(pyomo_model.model_dump_json(indent=2))
-        with open(
-            Path(__file__).parent.parent / "calculations/mock_instructor.json", "w"
-        ) as f:
-            f.writelines(pyomo_model.model_dump_json(indent=2))
-    except ValidationError as e:
-        print(e)
-    except ValueError as e:
-        print(e.args)
+    print(
+        ask_llm_for_pyomo_model(
+            post_office_optimization_task,
+            validate_input=False,
+            max_retries=1,
+            mock=True,
+        ).model_dump_json(indent=2)
+    )
